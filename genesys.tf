0ad89692cb8e02e29138af1b7f2e29968522611e
Naga Praneeth <mandali.praneeth@gmail.com>
2025-04-01 17:58:38 +0530

    Added a new Genesys file

---
+++ genesys.tf
@@ -0,0 +1,12 @@
+variable "genesyscloud_client_id" {
+  type = string
+}
+
+variable "genesyscloud_client_secret" {
+  type = string
+}
+
+variable "genesyscloud_region" {
+  type    = string
+  default = "eu-central-1" # Change to your region
+}
